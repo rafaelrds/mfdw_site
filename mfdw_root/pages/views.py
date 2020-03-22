3c3564526fd72d5b0bc2ca583d85b365de46c0bb
Rafael Rego <crafards@gmail.com>
2020-03-22 15:31:01 -0300

    extract urls.py file to inside the app 'pages'

--- mfdw_root/pages/views.py
+++ mfdw_root/pages/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+
 
 # Create your views here.
+def index(request):
+    return HttpResponse("<h1>The Meandco Homepage</h1>")
